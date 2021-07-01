6fb4d070946564373ab31bb7a4aa05a32d65bcf9
agcastro1994 <agcastro1994@gmail.com>
2021-07-01 15:18:49 -0300

    backend working, first front end trials

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,17 +1,17 @@
 from flask import Flask, request,render_template
 from flask import jsonify
 from flask_cors import CORS
-from flask_restful import Api, Resource
 import urllib.parse
 from .tasks.models import db, Task
 
 
 app = Flask(__name__)
+CORS(app, resources={r'/*': {'origins': '*'}})
 # Configure Database URI:
 params = urllib.parse.quote_plus("DRIVER={SQL Server};SERVER=testing-azure.database.windows.net;DATABASE=toDoApp;UID=agcastro1994;PWD=art041,,,")
 
 SECRET_KEY = '123447a47f563e90fe2db0f56b1b17be62378e31b7cfd3adc776c59ca4c75e2fc512c15f69bb38307d11d5d17a41a7936789'
-PROPAGATE_EXCEPTIONS = True
+#PROPAGATE_EXCEPTIONS = True
 # Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = "mssql+pyodbc:///?odbc_connect=%s" % params
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,8 +20,7 @@ app.config['ERROR_404_HELP'] = False
 
 
 if __name__ == '__main__':
-    app.debug = False
-    app.run(host='0.0.0.0', port=4996)
+    app.debug = True
 
 #Flask REST Api code
 # api = Api(app)
